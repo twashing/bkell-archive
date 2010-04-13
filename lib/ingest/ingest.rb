6e32377296791336c3dbe9f21ba50dfffd2bedc7
U-RCI\timothy.washington <timothy.washington@LRMIOMP0364.rci.rogers.ca>
2010-04-13 14:20:50 -0400

    adding basic code and test structure to repo

---
+++ lib/ingest/ingest.rb
@@ -0,0 +1,19 @@
+
+Module Ingest 
+	
+	
+	##
+	# Infer the type of the incoming documents 
+	class Ingest 
+		
+		## 
+		# 1. location URI of file/directory -> get 
+		# 2. working location to put the files ( from configuration ) 
+		def execute(sourceURI) 
+			
+			
+		end
+		
+	end
+	
+end 
---
+++ lib/transform/transform.rb
@@ -0,0 +1,17 @@
+
+Module Transform
+	
+	class Transform 
+		
+		## 
+		# inputs specify the 
+		# 1. source location of the files 
+		# 2. type, mapped to Baron: DCR, RSS, NewsML
+		def execute( sourceURI, baronMapping ) 
+			
+			
+		end
+		
+	end
+		
+end
