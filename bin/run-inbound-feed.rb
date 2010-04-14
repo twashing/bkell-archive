6896ef0ea70ba649b82722d919f67d7f5b8a1d8f
Joe <joe.hohertz@rci.rogers.com>
2010-04-14 00:56:53 -0400

    An initial "run inbound feed" script which puts together some of the concepts so far.
    Nevermind some of the odd config (using radio data in a canadianbusiness config file)
    
    point the basedir in the YAML config to your data, ensure something is < 24 hours old,
    and run:
    
    ruby bin/run-inbound-feed.rb www_canadianbusiness_com
    
    It will dump the config loaded, and list all files newer in the basedir or any decendant
    directory in it's present form...
    
    Much much more to do...

---
+++ bin/run-inbound-feed.rb
@@ -0,0 +1,29 @@
+
+# prototype inbound feed runner
+# everything subject to change.
+
+$BARON_BASE = File.dirname(File.dirname(File.expand_path($PROGRAM_NAME)))
+$LOAD_PATH.unshift($BARON_BASE + '/lib')
+
+require 'pp'
+require 'yaml'
+require 'Baron'
+
+configName = ARGV[0]
+raise "Must specify configuration to run" if configName == nil || configName == ""
+
+configFileName = $BARON_BASE + "/etc/infeeds/#{configName}.yaml"
+pp configFileName
+raise "Unknown configuration #{configName}" if ! File.exists? configFileName
+
+cfg = YAML.load_file(configFileName)
+pp cfg
+
+thisdir = cfg['teamsiteDcrFiles']['basedir']
+pp thisdir
+
+myiter = Baron::Feed::NewFileSource.new(thisdir)
+pp myiter.filelist
+
+__END__
+
