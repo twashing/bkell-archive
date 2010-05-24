8a3f64324db54651b2b2a0e9025708cc01897781
Timothy Washington <timothyw@frye.local>
2010-05-24 09:47:36 -0400

    -- options vs. xpath input to load command -- completed forking if blocks for OPTIONS vs. XPATH input

--- bkell.clj
+++ bkell.clj
@@ -25,12 +25,14 @@
 	 (caseALoginCommand3 [node] 
 	    (println (str "caseALoginCommand3: " node))
 	 )
+	 
+	 ;; PRINT command 
 	 (caseAPrintCommand6 [node] 
 	    (println (str "caseAPrintCommand6: " node)) 
 	 )
 	 
 	 
-	 ;; load command 
+	 ;; LOAD command 
 	 (caseALoadCommand3 [node] 
 	    (println (str "caseALoadCommand3: " node)) 
 	    
@@ -38,7 +40,7 @@
 	    
 	    
 	    (proxy-super inALoadCommand3 node)   ;; inALoadCommand3(node);
-		  
+	    
 	    (if (not= (. node getLoad ) nil)	 ;; if(node.getLoad() != null) { node.getLoad().apply(this); }
 	       (.. node getLoad (apply this) ) 
 	    )
@@ -46,28 +48,41 @@
 	    (if (not= (. node getLbracket ) nil) ;; if(node.getLbracket() != null) { node.getLbracket().apply(this); }
 	       (.. node getLbracket (apply this) ) 
 	    )
-        
+	     
 	    (if (not= (. node getCommandInput ) nil) ;; if(node.getCommandInput() != null) { node.getCommandInput().apply(this); }
-	       (.. node getCommandInput (apply this) ) 
+	       
+	       (do ;; execute 'if' block 
+		  (.. node getCommandInput (apply this) ) 
+		  
+		  ;; TODO - AOptsCommandInput ... 
+		  (if (instance? com.interrupt.bookkeeping.cc.node.AOptsCommandInput (. node getCommandInput) ) 
+		     (do (println "OPTIONS input"))
+		  ) 
+		  
+		  ;; TODO - AXpathCommandInput ... 
+		  (if (instance? com.interrupt.bookkeeping.cc.node.AXpathCommandInput (. node getCommandInput) ) 
+		     
+		     (println "XPATH input")
+		  ) 
+	       )
 	    )
-        
+	     
 	    (if (not= (. node getRbracket ) nil) ;; if(node.getRbracket() != null) { node.getRbracket().apply(this); }
 	       (.. node getRbracket (apply this) ) 
 	    )
-        
-		  
+	    
+	    
 	    (proxy-super outALoadCommand3 node)  ;; outALoadCommand3(node);
-
+	    
 	 )
 	 
-	 ;; add command (for registering users too) 
+	 ;; ADD command (for registering users too) 
 		  ;; 1. check that there's not an existing user 
 		  ;; 2. add corresponding default group to the new user 
 		  ;; 3. add to aauth.groups 
 		  ;; 4. add to aauth.users 
 		  ;; 5. add Associated Bookkeeping to Group 
 	 
-	 ;; 
 	 
 	)
 )
