0dd39f7548507a6e4168af703105a46e8bc4b8ef
Timothy Washington <timothyw@frye.local>
2010-08-25 12:09:43 -0400

    -- committing failed attempt to test callback code

--- test/bkell_test.clj
+++ test/bkell_test.clj
@@ -9,15 +9,28 @@
 	
 	(use 'depth_adapter)
 	
-	(let [agt (agent nil)] 
+	(comment 
+		(let [agt (agent java.lang.System/in)] 
+			
+			(send agt 
+				(fn [] 
+					(println "FINAL > " 
+						(with-out-str (bkell (get-depth-adapter)))))
+				nil
+			) 
+		)
 		
-		(send-off agt 
-			(fn [] (println "FINAL > " 
-				(with-out-str (bkell (get-depth-adapter)))))
-			nil
-		) 
+		(println "foobar")
 	)
 	
-	(println "foobar")	
+	(comment	;; TODO - try and test callback code with threads 
+		(def my-future
+		(future 
+			(bkell (get-depth-adapter)))
+		)
+		(println "foobar")
+		
+	)
+	
 )
 
