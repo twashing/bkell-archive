4a05d711ad2745024d7f3cbc3984ce71a292d832
Timothy Washington <twashing@gmail.com>
2010-11-02 19:45:02 -0700

    -- adding compojure dependency jars (incl. jetty) to project

--- project.clj
+++ project.clj
@@ -1,22 +1,12 @@
 
-(defproject bkell-clj 
+(defproject bkell-clj "pre-alpha"
    :description "A Clojure implementation of the bookkeeping project"
    
-   
-   ;;:repositories  [   ["clojars" "http://clojars.org/repo"]
-	 ;;  						]
-   :dependencies  [  	[org.clojure/clojure-lang "1.2.0-RC3"]
-		     							[org.clojure/clojure-contrib "1.2.0-RC3"]
-		  						]
+   :dependencies  [  	;; [org.clojure/clojure-lang "1.2.0-RC3"]
+						;; [org.clojure/clojure-contrib "1.2.0-RC3"]
+                        [compojure "0.5.2"]
+                        [ring/ring-jetty-adapter "0.3.3"]
+				  ]
 	
-   :library-path  [ 	["lib/"]
-		     							["lib/zob"] ]
-   
-   (comment :compile-path "build"
-   
-   :java-source-path "src"
-   :javac-fork "true"
-   )
-   
 )
 
--- src/http/handler.clj
+++ src/http/handler.clj
@@ -1,8 +1,13 @@
 (ns http.handler
     (:use compojure.core)
-    (:require [compojure.route :as route]))
+    (:require [compojure.route :as route])
+    (:use ring.adapter.jetty)
+    )
 
 (defroutes core
-
+  (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+  (route/not-found "Page not found")
 )
+
+(run-jetty core {:port 8080})
 
