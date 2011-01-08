b048fed03006ae1ff43dfc925e26cf4e6e87a080
Timothy Washington <twashing@gmail.com>
2011-01-07 20:04:54 -0800

    -- adding compojure and rin / jetty-adapter to the project

--- project.clj
+++ project.clj
@@ -2,10 +2,12 @@
 (defproject bkell-clj "pre-alpha"
    :description "A Clojure implementation of the bookkeeping project"
    
-   :dependencies  [  	;; [org.clojure/clojure-lang "1.2.0-RC3"]
-						;; [org.clojure/clojure-contrib "1.2.0-RC3"]
+   :dependencies  [  	[org.clojure/clojure "1.2.0"]
+						[org.clojure/clojure-contrib "1.2.0"]
                         [compojure "0.5.2"]
                         [ring/ring-jetty-adapter "0.3.3"]
+                        [log4j/log4j "1.2.7"]
+                        [sablecc "3.2"]
 				  ]
 	
 )
