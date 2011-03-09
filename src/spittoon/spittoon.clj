36124eef544e4123fed0b0b5624e7ffc064db472
Timothy Washington <twashing@gmail.com>
2011-03-09 10:19:29 -0800

    -- tweaking get-mapping call -- more content to spittoon-test

--- src/spittoon/spittoon.clj
+++ src/spittoon/spittoon.clj
@@ -31,22 +31,27 @@ B) /system.main.system/aauthentication.main.aauthentication/users.aauth.users/<u
 
 A) /system.main.system/groups.main.groups/group.webkell/bookkeeping.main.bookkeeping/journals.main.journals/journal.generalledger/entries.main.entries/entry.qwertySTUB
 B) /system.main.system/groups.main.groups/<group.webkell>
-        doc & path ->   group.webkell/bookkeeping.main.bookkeeping/journals.main.journals/journal.generalledger/entries.main.entries/entry.qwertySTUB
+        doc & path ->   group.webkell/bookkeeping.main.bookkeeping/journals.main.journals/journal.generalledger/entries.main.entries/entry.qwertySTUB"
 
-"
   (:require clojure.contrib.str-utils2)
   (:require clojure.contrib.string)
-  (:require debug)
+  ;;(:require debug)
 )
 
 
 
-(defn get-mapping [location map-list]
+(defn get-mapping 
+  "For the eXist path A), we should get a mapping vector B) 
+  A) /system.main.system/aauthentication.main.aauthentication/groups.aauth.groups/group.webkell/user.root
+  B) ['/system.main.system/aauthentication.main.aauthentication/groups.aauth.groups/group.webkell', 'group.webkell/user.root']"
+
+  [location map-list]
   
   (loop [mitem (first map-list) mlist map-list ]
     
     (if (clojure.contrib.str-utils2/contains? location mitem)
-      [ mitem (last (clojure.contrib.string/split (re-pattern mitem) location)) ]
+      (let [leaf-item (last (clojure.contrib.string/split (re-pattern mitem) location))]
+        [ (str mitem (first (clojure.contrib.string/split #"/" leaf-item)) ) leaf-item ])
       (recur (first (rest mlist)) (rest mlist) ))
     
   )
--- test/spittoon_test.clj
+++ test/spittoon_test.clj
@@ -8,7 +8,7 @@
 (def map-list (:spittoon-mappings (load-file "etc/config/config.test.clj")))
 
 
-(deftest test-create
+(deftest test-mapping
   
   ;; test mapping lookup 
   (let [  result 
@@ -18,8 +18,65 @@
     
     (is (not (nil? result)))
     (is (= 2 (count result)))
+
+    (is (= "/system.main.system/aauthentication.main.aauthentication/groups.aauth.groups/group.webkell"
+            (nth result 0)))
+    (is (= "group.webkell/user.root" (nth result 1)))
     
   )
+)
+
+
+(deftest test-create 
+  
+  #_(spittoon/create exist-path xpath xdoc)
+  
+  ;; test create of a root document 
+  
+  ;; test create an element within a document
+  
+  ;; ERROR test create within an empty document 
+  
+  ;; ERROR test create over an existing document 
   
 )
+
+#_(deftest test-retrieve 
+  
+  (spittoon/retrieve exist-path xpath) 
+  
+  ;; test retrieve of existing document 
+  
+  ;; test retrieve of xpath within an existing document 
+  
+  ;; ERROR test retrieve of nil document 
+  
+)
+
+#_(deftest test-update 
+  
+  (spittoon/update exist-path xpath xdoc) 
+  
+  ;; test update of a root document 
+  
+  ;; test update an element within a document
+  
+  ;; ERROR test update within an empty document 
+  
+)
+
+#_(deftest test-delete 
+  
+  (spittoon/delete exist-path xpath) 
+  
+  ;; test delete of a root document 
+  
+  ;; test delete of an element within a document 
+  
+  ;; ERROR test delete of a nil document 
+  
+)
+
+
+
 
