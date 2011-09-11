e41ec6238d38415c49951ab5efc64c5a9f7e7182
Timothy Washington <twashing@gmail.com>
2011-09-11 15:08:23 -0700

    -- re-adding core core backbone and jquery libraries: public/javascript/{backbone.js,import.js,jquery-1.6.3.js,json2.js,underscore.js} -- starting to refactor bkeeping.js to import backbone et. al. ; using phantomjs headles development

--- public/javascript/bkeeping.js
+++ public/javascript/bkeeping.js
@@ -1,8 +1,12 @@
 
 //require("/javascript/model/user.js", true);
+require("public/javascript/json2.js", true)
+require("public/javascript/underscore.js", true)
+require("public/javascript/backbone.js", true)
 
-var bkeeping = {} ;
+window.bkeeping = "tim";
 
-bkeeping.user = new User();
+console.log(window.bkeeping);
+//bkeeping.user = new User();
 
 
