8d2e07a819952dfc35c819fc7058fd45a30feea9
Timothy Washington <twashing@gmail.com>
2011-09-20 22:58:29 -0700

    -- adding outline for testing backbone user model

--- public/test/test_register.js
+++ public/test/test_register.js
@@ -10,6 +10,7 @@
 
 describe('Register', function () {
   
+  
   var register;
   beforeEach(function () {
     register = new bkeeping.models.Register;
@@ -27,71 +28,95 @@ describe('Register', function () {
   it("'backbone' Model should contain a 'urlRoot'", function () {
     expect(register.urlRoot).toEqual("/user"); 
   });
+});
+
+ 
+describe('Register w/ server interaction', function () {
   
-  // test with an empty password - should ERROR 
-  it("empty password should return an error", function () { 
+  var register;
+  beforeEach(function() {
+    register = new bkeeping.models.Register;
+  });
+  afterEach(function() {  
+      
+      // ** delete the user created on the DB 
+  });
+  
+  // test creating a user 
+  it("do a basic save, and error on duplication attempt", function () { 
    
     register.set( 
       { "tag": "user",
         "username": "stub", 
         "password": "stub", 
       },
-      {silent: true});
+      { silent: true });
     
     register.save ( {}, 
                     { success : function(model, response) { 
-                        console.log("success CALLED > model["+ model +"] > response["+ response +"]"); 
+                        
+                        console.log("success CALLED > model["+ model +"] > response["+ response.responseText +"]"); 
+                        
+                        // assert that the success is getting called 
+                        expect(response.responseText).toBeDefined();
+
+                        // assert that the user is getting returned 
+                        var ruser = eval(response.responseText);
+                        expect(ruser.tag).toEqual("user");
+                        
+                        // ** assert that we got a 300 redirect to the landing page 
+                        
                       },
                       error : function(model, response) { 
-                        //var thing = 1
-                        //console.log("Error CALLED")
                         console.log("error CALLED > model["+ model +"] > response["+ response.responseText +"]"); 
                       }
                     }
-                  ); 
+                  );
+    
+    // ** test adding a duplicate user 
+    register.save ( {}, 
+                    { success : function(model, response) { 
+                        
+                        console.log("success CALLED > model["+ model +"] > response["+ response.responseText +"]"); 
+                        
+                      },
+                      error : function(model, response) { 
+                        console.log("error CALLED > model["+ model +"] > response["+ response.responseText +"]"); 
+                        
+                        // ** assert that error is getting called
+                        expect(response.responseText).toBeDefined();
+
+                      }
+                    }
+                  );
     
   });
   
-  /*register.set( 
-    {"tag":"user",
-    "username":this.$("#username").val(), 
-    "password":this.$("#password").val(), 
-    "content":
-    [{"tag":"profileDetails",
-      "content":
-      [{"tag":"profileDetail",
-        "name":"first.name",
-        "value": this.$("#firstname").val(), 
-        "content":null},
-       {"tag":"profileDetail",
-        "name":"last.name",
-        "value": this.$("#lastname").val(), 
-        "content":null},
-       {"tag":"profileDetail",
-        "name":"email",
-        "value": this.$("#email").val(), 
-        "content":null},
-       {"tag":"profileDetail",
-        "name":"country",
-        "value": this.$("#country").val(), 
-        "content":null},
-       {"tag":"profileDetail",
-        "name":"currency",
-        "value": this.$("#currency").val(), 
-        "content":null}
-          
-    ]}]},
-    {silent: true});
-    */
-  // test with an empty email - should ERROR 
+  // ** test with an empty password - should ERROR 
+  it("empty password should return an error", function () { 
+  });
+  
+  // ** test with an empty email - should ERROR 
+  it("empty email should return an error", function () { 
+  });
+  
+  // ** test retrieving a user 
+  it("get the correct user", function () { 
+  });
+  
+  // ** test updating a user 
+  it("update the correct user", function () { 
+  });
+  
+  // ** test deleting a user 
+  it("delete the correct user", function () { 
+  });
+  
   
 });
 
 describe('Login', function() { 
 
-  /*it('this is a test', function() { 
-    expect(1).toEqual(1);
-  });
-  */
 });
+
 
