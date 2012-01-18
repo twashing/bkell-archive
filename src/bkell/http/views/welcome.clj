34acb0e4de8292f33ac6a57c83842d3047d8e977
Timothy Washington <twashing@gmail.com>
2012-01-18 13:27:31 -0800

    -- removed default welcome noir view(s) -- got default ROOT handler working

--- src/bkell/http/views/welcome.clj
+++
@@ -1,9 +0,0 @@
-(ns bkell.http.views.welcome
-  (:require [bkell.http.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to bkeeping"]))
--- src/bkell/run/run_ring.clj
+++ src/bkell/run/run_ring.clj
@@ -1,7 +1,6 @@
 (ns bkell.run.run-ring
   (:require [noir.server :as server]))
 
-#_(server/load-views "src/bkell/http/views/") 
 (server/load-views "src/bkell/http/") 
 
 (defn -main [& m]
