ab5722c46f112cca16f6a557a2218b4eefefe2bb
Timothy Washington <twashing@gmail.com>
2012-01-20 09:31:22 -0800

    -- updating update tests to reflect new directory structure

--- test/update_test.clj
+++ test/update_test.clj
@@ -1,10 +1,10 @@
 (ns update-test
-  (:use [clojure.test])
-  (:use somnium.congomongo)
-  (:require test-utils)
-  (:require commands.update)
-  (:require commands.get)
-  ;;(:require debug)
+  (:use [clojure.test]
+        [somnium.congomongo])
+  (:require [test-utils]
+            [bkell.commands.add :as addk]
+            [bkell.commands.update :as updatek]
+            [bkell.commands.get :as getk])
 )
 
 
@@ -17,7 +17,7 @@
   
   ;; error if user doesn't exist 
   (let  [ u1  (load-file "test/etc/data/stubu-one.clj")
-          ae  (try (commands/update-user u1)
+          ae  (try (updatek/update-user u1)
                   (catch java.lang.AssertionError ae ae))]
         
         (is (not (nil? ae)) "there SHOULD be an error if user doesn't exist")
@@ -28,8 +28,8 @@
   
   ;; assert that user was updated 
   (let  [ u1 (test-utils/add-user nil)
-          result  (commands/update-user (load-file "test/etc/data/stubu-one.clj"))
-          check   (first (commands/get-user "stub"))]
+          result  (updatek/update-user (load-file "test/etc/data/stubu-one.clj"))
+          check   (getk/get-user "stub")]
     
     (is (= "xxx" (:password check)) "user attributes have NOT changed")
   )
@@ -40,10 +40,10 @@
 (deftest test-update-currency-1
   
   ;; insert if currency doesn't exist 
-  (let [user (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [user (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         currency (load-file "test/etc/data/test-currency.clj")
-        rc0 (commands/update-currency currency "stub" false)
-        rc1 (commands/get-currency "stub" "AUD")]
+        rc0 (updatek/update-currency currency "stub" false)
+        rc1 (getk/get-currency "stub" "AUD")]
     (is (not (nil? rc1)) "1. we SHOULD have the 'AUD' currency")
     (is (= "AUD" (:id rc1)) "2. we SHOULD have the 'AUD' currency")
   )
@@ -51,10 +51,10 @@
 (deftest test-update-currency-2
   
   ;; assert that currency was updated 
-  (let [user (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [user (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         currency (load-file "test/etc/data/test-currency-CAD.clj")
-        rc0 (commands/update-currency currency "stub" false)
-        rc1 (commands/get-currency "stub" "CDN")]
+        rc0 (updatek/update-currency currency "stub" false)
+        rc1 (getk/get-currency "stub" "CDN")]
 
     (is (not (nil? rc1)) "we SHOULD have the 'CDN' currency")
     (is (= "CDN" (:id rc1)) "1. 'CDN' currency SHOULD have been updated")
@@ -71,11 +71,11 @@
 (deftest test-update-entry-1
   
   ;; insert if entry doesn't exist 
-  (let [user (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [user (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         ra (test-utils/populate-accounts)
         entry (load-file "test/etc/data/test-entry-FULL.clj")
-        re0 (commands/update-entry entry "stub")
-        re1 (commands/get-entry "stub" (:id entry))]
+        re0 (updatek/update-entry entry "stub")
+        re1 (getk/get-entry "stub" (:id entry))]
     (is (not (nil? re1)) "1. result entry should NOT be nil")
     (is (= "testid" (:id re1)) "2. entry SHOULD have the :id 'testid'")
   )
@@ -83,12 +83,12 @@
 (deftest test-update-entry-2
   
   ;; assert that entry was updated 
-  (let [user (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [user (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         ra (test-utils/populate-accounts)
         entry (load-file "test/etc/data/test-entry-FULL.clj")
-        re0 (commands/add-entry entry "stub")
-        re1 (commands/update-entry (merge entry { :date "06/30/2011" }) "stub" )
-        re2 (commands/get-entry "stub" (:id entry))]
+        re0 (addk/add-entry entry "stub")
+        re1 (updatek/update-entry (merge entry { :date "06/30/2011" }) "stub" )
+        re2 (getk/get-entry "stub" (:id entry))]
     
     (is (not (nil? re2)) "1. result entry should NOT be nil")
     (is (= "testid" (:id re2)) "2. entry SHOULD have the :id 'testid'")
@@ -105,8 +105,8 @@
   
   ;; assert that user was updated 
   (let  [ u1 (test-utils/add-user nil)
-          result  (commands/update (load-file "test/etc/data/stubu-one.clj"))
-          check   (first (commands/get-user "stub"))]
+          result  (updatek/update (load-file "test/etc/data/stubu-one.clj"))
+          check   (getk/get-user "stub")]
     
     (is (= "xxx" (:password check)) "user attributes have NOT changed")
   )
@@ -117,10 +117,10 @@
 (deftest test-updateC
   
   ;; assert that currency was updated 
-  (let [user (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [user (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         currency (load-file "test/etc/data/test-currency-CAD.clj")
-        rc0 (commands/update currency "stub" false)
-        rc1 (commands/get-currency "stub" "CDN")]
+        rc0 (updatek/update currency "stub" false)
+        rc1 (getk/get-currency "stub" "CDN")]
 
     (is (not (nil? rc1)) "we SHOULD have the 'CDN' currency")
     (is (= "CDN" (:id rc1)) "1. 'CDN' currency SHOULD have been updated")
@@ -133,12 +133,12 @@
 (deftest test-updateE
   
   ;; assert that entry was updated 
-  (let [user (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [user (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         ra (test-utils/populate-accounts)
         entry (load-file "test/etc/data/test-entry-FULL.clj")
-        re0 (commands/add-entry entry "stub")
-        re1 (commands/update (merge entry { :date "06/30/2011" }) "stub" )
-        re2 (commands/get-entry "stub" (:id entry))]
+        re0 (addk/add-entry entry "stub")
+        re1 (updatek/update (merge entry { :date "06/30/2011" }) "stub" )
+        re2 (getk/get-entry "stub" (:id entry))]
     
     (is (not (nil? re2)) "1. result entry should NOT be nil")
     (is (= "testid" (:id re2)) "2. entry SHOULD have the :id 'testid'")
