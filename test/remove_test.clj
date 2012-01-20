e04ef24bdce0fb321639270a4464f9bada60bd8b
Timothy Washington <twashing@gmail.com>
2012-01-20 11:17:02 -0800

    -- updating remove tests to reflect new directory structure

--- test/remove_test.clj
+++ test/remove_test.clj
@@ -1,11 +1,10 @@
 (ns remove-test
-	(:use [clojure.test])
-    (:use somnium.congomongo)
-    (:require test-utils)
-    (:require commands.add)
-    (:require commands.get)
-    (:require commands.remove)
-    (:require debug)
+	(:use [clojure.test]
+          [somnium.congomongo])
+    (:require [test-utils]
+              [bkell.commands.add :as addk]
+              [bkell.commands.get :as getk]
+              [bkell.commands.remove :as removek])
 )
 
 
@@ -17,13 +16,13 @@
 (deftest test-remove-user 
 
   (let [result (test-utils/add-user nil)
-        r0 (commands/get-user "stub")]
+        r0 (getk/get-user "stub")]
 
       (is (not (nil? r0)) "a user SHOULD have been added")
-      (let [r1 (commands/remove-user "stub")
-            r2 (first (commands/get-user "stub"))
-            gg (first (commands/get-group "stub"))
-            bb (first (commands/get-bookkeeping "stub"))]
+      (let [r1 (removek/remove-user r0)
+            r2 (first (getk/get-user "stub"))
+            gg (first (getk/get-group "stub"))
+            bb (first (getk/get-bookkeeping "stub"))]
         (is (nil? r2) "user SHOULD have been deleted")
         (is (nil? gg) "group SHOULD have been deleted")
         (is (nil? bb) "bookkeeping SHOULD have been deleted")
@@ -35,14 +34,14 @@
 ;; get currency 
 (deftest test-remove-currency 
 
-  (let [result (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
-        rc (commands/get-currency "stub" "CDN")]
+  (let [result (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
+        rc (getk/get-currency "stub" "CDN")]
     
     (is (not (nil? rc)) "currency result should NOT be nil")
     (is (= "CDN" (:id rc)) "There SHOULD be a 'CDN' currency with the username 'stub'")
 
-    (let [zz (commands/remove-currency "stub" "CDN")
-          rm (commands/get-currency "stub" "CDN")]
+    (let [zz (removek/remove-currency { :id "CDN" } "stub")
+          rm (getk/get-currency "stub" "CDN")]
       (is (nil? rm) "currency SHOULD be nil")
     )
   )
@@ -52,15 +51,15 @@
 ;; get account 
 (deftest test-remove-account
   
-  (let [result (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [result (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         xx (test-utils/populate-accounts)
-        ra (commands/get-account "stub" "cash")]
+        ra (getk/get-account "stub" "cash")]
     
     (is (not (nil? ra)) "cash result should NOT be nil")
     (is (= "cash" (:id ra)) "There SHOULD be a 'cash' account with the username 'stub'")
     
-    (let [zz (commands/remove-account "stub" "cash")
-          ra (commands/get-account "stub" "cash")]
+    (let [zz (removek/remove-account { :id "cash" } "stub")
+          ra (getk/get-account "stub" "cash")]
       (is (nil? ra) "account SHOULD be nil")
     )
   )
@@ -70,16 +69,16 @@
 ;; get entry 
 (deftest test-remove-entry
 
-  (let [result (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [result (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         xx (test-utils/populate-accounts)
         yy (test-utils/populate-entries)
-        re (commands/get-entry "stub" "testid")]
+        re (getk/get-entry "stub" "testid")]
     
     (is (not (nil? re)) "entry result should NOT be nil")
     (is (= "testid" (:id re)) "There SHOULD be a 'testid' entry with the username 'stub'")
     
-    (let [zz (commands/remove-entry "stub" "testid")
-          re (commands/get-entry "stub" "testid")]
+    (let [zz (removek/remove-entry { :id "testid" } "stub")
+          re (getk/get-entry "stub" "testid")]
       (is (nil? re) "entry SHOULD be nil")
     )
   )
@@ -91,13 +90,13 @@
 (deftest test-removeU
 
   (let [result (test-utils/add-user nil)
-        r0 (commands/get-user "stub")]
+        r0 (getk/get-user "stub")]
 
       (is (not (nil? r0)) "a user SHOULD have been added")
-      (let [r1 (commands/remove :user "stub")
-            r2 (first (commands/get-user "stub"))
-            gg (first (commands/get-group "stub"))
-            bb (first (commands/get-bookkeeping "stub"))]
+      (let [r1 (removek/removek { :tag :user :username "stub" })
+            r2 (first (getk/get-user "stub"))
+            gg (first (getk/get-group "stub"))
+            bb (first (getk/get-bookkeeping "stub"))]
         (is (nil? r2) "user SHOULD have been deleted")
         (is (nil? gg) "group SHOULD have been deleted")
         (is (nil? bb) "bookkeeping SHOULD have been deleted")
@@ -109,14 +108,14 @@
 ;; get currency 
 (deftest test-removeC
 
-  (let [result (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
-        rc (commands/get-currency "stub" "CDN")]
+  (let [result (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
+        rc (getk/get-currency "stub" "CDN")]
     
     (is (not (nil? rc)) "currency result should NOT be nil")
     (is (= "CDN" (:id rc)) "There SHOULD be a 'CDN' currency with the username 'stub'")
 
-    (let [zz (commands/remove :currency "stub" "CDN")
-          rm (commands/get-currency "stub" "CDN")]
+    (let [zz (removek/removek { :tag :currency :id "CDN" } "stub")
+          rm (getk/get-currency "stub" "CDN")]
       (is (nil? rm) "currency SHOULD be nil")
     )
   )
@@ -126,15 +125,15 @@
 ;; get account 
 (deftest test-removeA
   
-  (let [result (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [result (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         xx (test-utils/populate-accounts)
-        ra (commands/get-account "stub" "cash")]
+        ra (getk/get-account "stub" "cash")]
     
     (is (not (nil? ra)) "cash result should NOT be nil")
     (is (= "cash" (:id ra)) "There SHOULD be a 'cash' account with the username 'stub'")
     
-    (let [zz (commands/remove :account "stub" "cash")
-          ra (commands/get-account "stub" "cash")]
+    (let [zz (removek/removek { :tag :account :id "cash" } "stub")
+          ra (getk/get-account "stub" "cash")]
       (is (nil? ra) "account SHOULD be nil")
     )
   )
@@ -144,16 +143,16 @@
 ;; get entry 
 (deftest test-removeE
 
-  (let [result (commands/add-user (load-file "test/etc/data/stubu-two.clj"))
+  (let [result (addk/add-user (load-file "test/etc/data/stubu-two.clj"))
         xx (test-utils/populate-accounts)
         yy (test-utils/populate-entries)
-        re (commands/get-entry "stub" "testid")]
+        re (getk/get-entry "stub" "testid")]
     
     (is (not (nil? re)) "entry result should NOT be nil")
     (is (= "testid" (:id re)) "There SHOULD be a 'testid' entry with the username 'stub'")
     
-    (let [zz (commands/remove :entry "stub" "testid")
-          re (commands/get-entry "stub" "testid")]
+    (let [zz (removek/removek { :tag :entry :id "testid" } "stub")
+          re (getk/get-entry "stub" "testid")]
       (is (nil? re) "entry SHOULD be nil")
     )
   )
