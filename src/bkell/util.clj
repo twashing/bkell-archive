82e8f3a114e5fa67f6ecd1086c2a2dc2372f80c7
Timothy Washington <twashing@gmail.com>
2012-03-29 17:30:33 -0700

    -- adding a server function to generate system wide unique ID

--- src/bkell/util.clj
+++ src/bkell/util.clj
@@ -4,6 +4,13 @@
 )
 
 
+(defn generate-id 
+  "generate system wide unique ID"
+  []
+  (.. (java.rmi.dgc.VMID.) toString (replaceAll ":" "") )
+)
+
+
 ;; Inspired by Shantanu Kumar's util functions - https://bitbucket.org/kumarshantanu/clj-miscutil/src/acfb97c662d9/src/main/clj/org/bituf/clj_miscutil.clj
 (defn illegal-arg
   "Throw IllegalArgumentException with specified arguments. Use this when you
