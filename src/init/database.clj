b19471e853e207b5e759c08396e6cc6cca11773e
Timothy Washington <twashing@gmail.com>
2012-04-28 07:39:17 -0700

    -- adding "src/init/database.clj" -- adding init function that sets up the database; adding currencies in this case

---
+++ src/init/database.clj
@@ -0,0 +1,32 @@
+(ns init.database
+  (:use somnium.congomongo)
+)
+
+(defn setup [] 
+  
+  (somnium.congomongo/mongo! :db "bkell") ;; connect to mongodb
+  
+  (let [  currency-list
+          [
+            { :tag :currency :id "CDN" :name "Canadian Dollar" :content [] }
+            { :tag :currency :id "USD" :name "United States Dollar" :content [] }
+            { :tag :currency :id "EUR" :name "European Euro" :content [] }
+            { :tag :currency :id "GBP" :name "Great British Pound" :content [] }
+            { :tag :currency :id "YEN" :name "Japanese Yen" :content [] }
+            { :tag :currency :id "AUD" :name "Australian Dollar" :content [] }
+            { :tag :currency :id "ZAR" :name "South African Rand" :content [] }
+            { :tag :currency :id "CNY" :name "Chinese Yuan Renminbi" :content [] }
+            { :tag :currency :id "RUB" :name "Russian Ruble" :content [] }
+            { :tag :currency :id "BRL" :name "Brazilian Ruble" :content [] }
+            { :tag :currency :id "INR" :name "Indian Rupee" :content [] }
+          ]
+        ]
+
+    (map 
+      #(insert! :currencies %1)
+      currency-list
+    )
+  )
+  
+)
+
