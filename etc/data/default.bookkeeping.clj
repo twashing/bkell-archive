3de967340c559accffe31adaf29a01ffc49ee3d9
Timothy Washington <twashing@gmail.com>
2012-05-01 22:48:29 -0700

    -- adding 4 default accounts for new users

--- etc/data/default.bookkeeping.clj
+++ etc/data/default.bookkeeping.clj
@@ -28,7 +28,36 @@
      :content nil}]}
   { :tag :accounts 
     :id "main.accounts" 
-    :content [ ]}
+    :content 
+    [{
+       :counterWeight : "debit"
+       :name : "cash"
+       :type : "asset"
+       :id : "cash"
+       :tag : "account"
+     }
+     {
+       :counterWeight : "credit"
+       :name : "expense"
+       :type : "expense"
+       :id : "expense"
+       :tag : "account"
+     }
+     {
+       :counterWeight : "debit"
+       :name : "revenue"
+       :type : "revenue"
+       :id : "revenue"
+       :tag : "account"
+     }
+     {
+       :counterWeight : "credit"
+       :name : "accounts payable"
+       :type : "liability"
+       :id : "accounts payable"
+       :tag : "account"
+     }
+    ]}
   {:tag :journals
    :id "main.journals"
    :content
