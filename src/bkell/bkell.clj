a4391466c0ecc1f689977220f39cddbf08dc4d06
Timothy Washington <twashing@gmail.com>
2012-06-16 11:56:48 -0700

    -- some debug statements

--- src/bkell/bkell.clj
+++ src/bkell/bkell.clj
@@ -22,7 +22,10 @@
         database (-> config mode :database)]
     
     ;; connect to the DB server
+    (println (str "Connecting to DB url[" dburl  "] / port[" dport  "]"))
     (mg/connect! { :host dburl :port dport }) 
+    
+    (println (str "Setting Database[" database "]"))
     (mg/set-db! (mg/get-db database))    ;; select the DB 
   )
   (def shell (ref { :active true })) 	;; the shell and memory 
