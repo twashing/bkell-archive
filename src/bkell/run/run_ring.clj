2dde0f1b12a64f47b6bcc5b1259422fac5f7260c
Timothy Washington <twashing@gmail.com>
2012-06-16 18:38:33 -0700

    -- trying to get proper http port config for PROD (for heroku)

--- src/bkell/run/run_ring.clj
+++ src/bkell/run/run_ring.clj
@@ -27,7 +27,7 @@
     ;; Setting the mode in the shell 
     (dosync 
       (alter bkell/shell conj { :mode mode })
-      (alter bkell/shell conj (merge config (merge (:prod config) {:host-port host-port}))
+      (alter bkell/shell conj (merge config (merge (:prod config) {:host-port host-port}) ))
     )
     
     (println (str "Bkell: " @bkell/shell))
