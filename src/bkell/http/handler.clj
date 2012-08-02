b4291ecb2bd10c4028a5c679c99be7ec08c45c48
Timothy Washington <twashing@gmail.com>
2012-08-01 18:21:37 -0700

    -- successfully branching to cfinstall.html vs. index.html pages based on user-agent

--- src/bkell/http/handler.clj
+++ src/bkell/http/handler.clj
@@ -106,28 +106,10 @@
     )
   )
 )
-(def gocfinstall []
-  (let  [ templ (enlive/html-resource "cfinstall.html")
-          mode (:mode @bkell/shell)
-          host-url (-> mode (@bkell/shell) :host-url)
-          host-port (-> mode (@bkell/shell) :host-port)
-          developer-key (-> mode (@bkell/shell) :developer-key)
-          ruri  (str  (generate-host-address host-url (if (= mode :dev) host-port nil)) "/callbackGitkit" ) ;; conditionally assign the host-port
-        ]
+(defn gocfinstall []
+  (let  [ templ (enlive/html-resource "include/cfinstall.html") ]
     
-    ;; get (-> raw-request :headers "user-agent")
-    (println (str "/ ROOT HANDLER: " (request/ring-request)))
-    (let [raw-request (request/ring-request)
-          uagent (get (:headers raw-request) "user-agent")
-          ]
-
-      ;; if IE, then return a ChromeFrameInstall page, otherwise, go to index
-      (println (str "/ ROOT user-agent: " uagent " / contains 'MSIE' [" (re-find #"MSIE" uagent) "] "))
-      (if (re-find #"MSIE" uagent)
-        (gocfinstall)
-        (goindex)
-      )
-    )
+    (apply str (enlive/emit* templ))
   )
 )
 
@@ -155,54 +137,6 @@
         (gocfinstall)
         (goindex)
       )
-    )
-    
-    (apply str (enlive/emit*  (enlive/transform 
-                                templ
-                                [[ :script (enlive/nth-of-type 10) ]]  ;; get the 3rd script tag 
-                                (enlive/content 
-                                  (str 
-                                    " 
-                                          //<![CDATA[
-                                            $(document).ready(function() {
-                                              
-                                              /*********
-                                               * Some internal code is giving the body a margin - can't figure out what it is
-                                               *********/
-                                              $('body').css('margin', '0px'); 
-                                              
-                                              
-                                              /*********
-                                               * GITkit Account Chooser code
-                                               *********/
-                                              window.google.identitytoolkit.setConfig({
-                                                developerKey: '" developer-key "',
-                                                companyName: 'Interrupt Software Inc.',
-                                                callbackUrl: '" ruri "',
-                                                realm: '', 
-                                                userStatusUrl: '/userStatusUrl',
-                                                loginUrl: '/loginUrl',
-                                                signupUrl: '/register',
-                                                homeUrl: '/landing',
-                                                logoutUrl: '/logout',
-                                                idps: ['Gmail', 'Yahoo', 'AOL'],
-                                                tryFederatedFirst: true,
-                                                useCachedUserStatus: false
-                                              });
-                                               
-                                              $('#account-chooser').accountChooser();
-                                              
-                                              
-                                              /********* 
-                                               * load the footer file
-                                               *********/
-                                              $('#footer').load('/include/footerPart.html');
-                                              
-                                            });
-                                          //]]>
-                                    "
-                                  ))
-              ))
     )
   )
 )
