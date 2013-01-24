e5b363dfbb090029abebf9e454df6d02d51c0daf
Murtaza Husain <murtaza.husain@searce.com>
2013-01-24 09:37:20 +0530

    removed hardcoding of domain

--- etc/config/config.clj
+++ etc/config/config.clj
@@ -4,7 +4,7 @@
   :dev {
     :host-url "localhost"
     :host-port "8080"
-    :developer-key "AIzaSyDc7_lGZsmbtdOUpprPClKBOxXCQ6LztRE"
+    ;:developer-key "AIzaSyDc7_lGZsmbtdOUpprPClKBOxXCQ6LztRE"
     :host-url-db "localhost"
     :host-port-db 27017
     :database "bkell"
@@ -13,7 +13,6 @@
     :host-url "bkeeping.com"
     :host-port "8888"
     :developer-key "AIzaSyDc7_lGZsmbtdOUpprPClKBOxXCQ6LztRE"
-
     ; url "heroku_app2227080:pvqdogtk4tptcl8e4p8kt7t1al@ds029297.mongolab.com:29297/heroku_app2227080"
     ;:host-url-db "ds029297.mongolab.com:29297"
     :host-url-db "mongodb://heroku_app2227080:pvqdogtk4tptcl8e4p8kt7t1al@ds029297.mongolab.com:29297/heroku_app2227080"
