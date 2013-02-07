a0cfbab5428ce2a5494e3a77bf06b38f089797a8
Timothy Washington <twashing@gmail.com>
2013-02-06 19:36:06 -0800

    -- adding back handler (goindex) functions

--- project.clj
+++ project.clj
@@ -4,7 +4,12 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]
                  [com.cemerick/friend "0.1.3"]
-                 [ring/ring-jetty-adapter "1.2.0-SNAPSHOT"]]
+                 [ring/ring-jetty-adapter "1.2.0-SNAPSHOT"]
+                 [org.clojure/core.incubator "0.1.2"]
+                 [enlive "1.0.1"]
+                 [com.novemberain/monger "1.0.0-SNAPSHOT"]
+                 [org.clojure/core.match "0.2.0-alpha9"]
+                 ]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler thing.handler/app}
   :profiles
--- src/bkell/http/handler.clj
+++ src/bkell/http/handler.clj
@@ -1,13 +1,102 @@
 (ns bkell.http.handler
-  (:use compojure.core)
+  (:use [compojure.core]
+        [clojure.core.strint])
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [cemerick.friend :as friend]
+            [net.cgrand.enlive-html :as enlive]
+            [bkell.bkell :as bkell]
+            ))
+
+
+(defn generate-host-address [host-url host-port]
+  (str  "http://"
+        (if (-> host-url nil? not) host-url "localhost")
+        (if (-> host-port nil? not) (str ":" host-port))
+        )
+)
+(defn goindex []
+  (let  [templ (enlive/html-resource "index.html")
+         mode (:mode @bkell/shell)
+         host-url (-> mode (@bkell/shell) :host-url)
+         host-port (-> mode (@bkell/shell) :host-port)
+         developer-key (-> mode (@bkell/shell) :developer-key)
+
+         xx (println (<< "(xfn ~{host-url} ~{(if (= mode :dev) host-port nil)})"))
+         ruri  (str  (generate-host-address host-url (if (= mode :dev) host-port nil)) "/callbackGitkit" ) ;; conditionally assign the host-port
+        ]
+
+    (apply str (enlive/emit*  (enlive/transform
+                                templ
+                                [[ :script (enlive/nth-of-type 11) ]]  ;; get the 3rd script tag
+                                (enlive/content
+                                  (str
+                                    "
+                                          //<![CDATA[
+                                            $(document).ready(function() {
+
+                                              /*********
+                                               * Some internal code is giving the body a margin - can't figure out what it is
+                                               *********/
+                                              $('body').css('margin', '0px');
+
+
+                                              /*********
+                                               * GITkit Account Chooser code
+                                               *********/
+                                              window.google.identitytoolkit.setConfig({
+                                                developerKey: '" developer-key "',
+                                                companyName: 'Interrupt Software Inc.',
+                                                callbackUrl: '" ruri "',
+                                                realm: '',
+                                                userStatusUrl: '/userStatusUrl',
+                                                loginUrl: '/loginUrl',
+                                                signupUrl: '/register',
+                                                homeUrl: '/landing',
+                                                logoutUrl: '/logout',
+                                                idps: ['Gmail', 'Yahoo', 'AOL'],
+                                                tryFederatedFirst: true,
+                                                useCachedUserStatus: false
+                                              });
+
+                                              $('#account-chooser').accountChooser();
+
+
+                                              /*********
+                                               * load the footer file
+                                               *********/
+                                              $('#footer').load('/include/footerPart.html');
+
+                                            });
+                                          //]]>
+                                    "
+                                  ))
+              ))
+    )
+  )
+)
+
 
 (defroutes app-routes
-  (GET "/" [:as request]
+
+
+  #_(GET "/" [:as request]
        (throw (Exception. "fubar"))
        "Hello World")
-  (route/not-found "Not Found"))
+  #_(route/not-found "Not Found")
+
+
+  ;; ======
+  ;; ROOT Page
+  (GET "/" []   ;; index is the default page of the application
+    (goindex)
+  )
+
+
+)
+
+
 
 (def app
-  (handler/site app-routes))
+  (handler/site
+   (friend/authenticate app-routes {})))
