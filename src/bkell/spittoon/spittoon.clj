9df0368957c3d0aaf393374aa6620a970969061c
Timothy Washington <twashing@gmail.com>
2013-02-06 19:14:16 -0800

    -- adding back bkell src codebase

---
+++ src/bkell/spittoon/spittoon.clj
@@ -0,0 +1,91 @@
+(ns bkell.spittoon.spittoon
+
+"Spittoon is functionality to convert (CRUD) calls between 
+    1. absolute XML path, to 
+    2. a mapped XML path 
+
+
+MAPPINGS 
+/system.main.system/aauthentication.main.aauthentication/groups.aauth.groups/group.* 
+/system.main.system/aauthentication.main.aauthentication/users.aauth.users/user.* 
+/system.main.system/groups.main.groups/group.* 
+
+
+CREATE - will create the node in the mapped path 
+RETREVE - will retrieve the node from the mapped path 
+UPDATE - will update the node at the mapped path 
+DELETE - will delete the node from the mapped path 
+
+
+For some examples, the absolute XML path A) will be mapped to a relative path B) 
+
+A) /system.main.system/aauthentication.main.aauthentication/groups.aauth.groups/group.webkell/user.root
+B) /system.main.system/aauthentication.main.aauthentication/groups.aauth.groups/<group.webkell>
+        doc & path ->   group.webkell/user.root
+
+
+A) /system.main.system/aauthentication.main.aauthentication/users.aauth.users/user.root/profileDetails.user.details
+B) /system.main.system/aauthentication.main.aauthentication/users.aauth.users/<user.root>
+        doc & path ->   user.root/profileDetails.user.details
+
+
+A) /system.main.system/groups.main.groups/group.webkell/bookkeeping.main.bookkeeping/journals.main.journals/journal.generalledger/entries.main.entries/entry.qwertySTUB
+B) /system.main.system/groups.main.groups/<group.webkell>
+        doc & path ->   group.webkell/bookkeeping.main.bookkeeping/journals.main.journals/journal.generalledger/entries.main.entries/entry.qwertySTUB"
+
+  (:require clojure.contrib.str-utils2)
+  (:require clojure.contrib.string)
+  ;;(:require debug)
+)
+
+
+
+(defn get-mapping 
+  "For the eXist path A), we should get a mapping vector B) 
+  A) /system.main.system/aauthentication.main.aauthentication/groups.aauth.groups/group.webkell/user.root
+  B) ['/system.main.system/aauthentication.main.aauthentication/groups.aauth.groups/group.webkell', 'group.webkell/user.root']"
+
+  [location map-list]
+  
+  (loop [mitem (first map-list) mlist map-list ]
+    
+    (if (clojure.contrib.str-utils2/contains? location mitem)
+      (let [leaf-item (last (clojure.contrib.string/split (re-pattern mitem) location))]
+        [ (str mitem (first (clojure.contrib.string/split #"/" leaf-item)) ) leaf-item ])
+      (recur (first (rest mlist)) (rest mlist) ))
+    
+  )
+)
+
+
+(defn string-xpath-part [part]  ;; convert "fu.bar" into fu[@id='bar']
+    
+    (let [ ml (clojure.contrib.string/split #"\." part) ] 
+        (str    (first ml) 
+            "[@id='" 
+            (clojure.contrib.str-utils2/join "." (rest ml)) 
+            "']" ) 
+    ) 
+) 
+
+(defn xpath-from-epath 
+"We want to return a normal XPath chunk like so: 
+    group.webkell/user.root   ->  group[@id='webkell']/user[@id='root'] "
+    [epath]
+    
+    (let [rlist (clojure.contrib.string/split #"/" epath)]
+        ;;(debug/debug-repl)
+        (reduce 
+            (fn [a b] 
+                (str    a 
+                    (str "/" (string-xpath-part b))
+                )) 
+            (string-xpath-part (first rlist)) 
+            (rest rlist) )
+    )
+)
+
+
+
+
+
---
+++ src/bkell/util.clj
@@ -0,0 +1,62 @@
+(ns bkell.util
+  
+  #_(:require [swank.core]
+            [swank.core.connection])
+)
+
+
+(defn generate-id 
+  "generate system wide unique ID"
+  []
+  (.. (java.rmi.dgc.VMID.) toString (replaceAll ":" "") (replaceAll "-" ""))
+)
+
+
+;; Inspired by Shantanu Kumar's util functions - https://bitbucket.org/kumarshantanu/clj-miscutil/src/acfb97c662d9/src/main/clj/org/bituf/clj_miscutil.clj
+(defn illegal-arg
+  "Throw IllegalArgumentException with specified arguments. Use this when you
+   encounter bad/invalid parameters."
+  [reason] 
+  (throw (IllegalArgumentException. reason ))
+)
+
+
+(defmacro verify-arg
+  "Like assert, except for the following differences:
+  1. does not check for *assert* flag
+  2. throws IllegalArgumentException"
+  [arg reason]
+  `(if ~arg 
+      true
+     (illegal-arg '~reason)))
+
+;;(verify-arg false "fubar")
+
+
+;; doing this to avoid a 'Cyclic load dependency' Exception 
+(defn get-bkell[]
+  (->> "shell" symbol (ns-resolve (symbol "bkell.bkell") ))
+)
+
+
+(defn generate-error-response [ msg ]
+  (merge { :tag :error } { :message msg }))
+(defn generate-error-responses [ & msgs ]
+  { :tag :errors 
+    :content  (reduce #(conj %1 (generate-error-response %2))
+                [] msgs)
+  }
+)
+
+(defn wrap-error [err status]
+  
+  (let [stat (if (-> status nil? not) status 400)]
+    { :status stat :headers { "Content-Type" "application/json" } :body err })
+)
+(defn wrap-error-msg [msg status]
+  (wrap-error (generate-error-response msg) status))
+
+#_(def swank-con swank.core.connection/*current-connection*)
+#_(defmacro break []
+  `(binding [swank.core.connection/*current-connection* swank-con]
+     (swank.core/break)))
