c3c73182f88be36ca9184e78f42b459c3ed270ae
Timothy Washington <twashing@gmail.com>
2014-02-15 21:25:42 -0800

    -- serving index through handler.clj -- adding server run function

--- project.clj
+++ project.clj
@@ -2,9 +2,15 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
-  :plugins [[lein-ring "0.8.10"]]
+                 [compojure "1.1.6"]
+                 ]
+
   :ring {:handler bkell.handler/app}
+
+  :resources-path "resources/public"
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]
+
+                        [ring/ring-jetty-adapter "1.2.1"]
+                        [alembic "0.2.1"]]}})
--- src/bkell/handler.clj
+++ src/bkell/handler.clj
@@ -1,12 +1,20 @@
 (ns bkell.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [ring.util.response :as ring-resp]))
+
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/resources "/")
-  (route/not-found "Not Found"))
+
+ (GET "/" []
+
+      (-> (ring-resp/response (slurp (io/resource "public/index.html")))
+          (ring-resp/content-type "text/html")))
+
+ (route/resources "/")
+ (route/not-found "Not Found"))
 
 (def app
   (handler/site app-routes))
