f632db3e594e1ff7b3c452300a8e9c52f707f569
Timothy Washington <twashing@gmail.com>
2014-02-19 17:24:24 -0800

    -- changing references to mirror PROD environment

--- src/bkell/handler.clj
+++ src/bkell/handler.clj
@@ -12,8 +12,12 @@
       (-> (ring-resp/response (slurp (io/resource "public/index.html")))
           (ring-resp/content-type "text/html")))
 
+ #_(route/files "/" {:root "resources/public/"})
+ #_(route/resources "/" {:root "resources/public/"})
+
  (route/resources "/")
  (route/not-found "Not Found"))
+
 
 (def app
   (handler/site app-routes))
