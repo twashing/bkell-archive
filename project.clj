14c41988932bda3fcc6ec8f4b731ba83dd99be20
Timothy Washington <twashing@gmail.com>
2014-02-20 14:07:29 -0800

    -- configuring source-paths and austin -- added "/signedUploadParams" compojure endpoint

--- project.clj
+++ project.clj
@@ -2,14 +2,19 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]
-                 [org.clojure/clojurescript "0.0-2173"]]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+
+                 [org.clojure/data.codec "0.1.0"]
+                 [compojure "1.1.6"]]
 
   :ring {:handler bkell.handler/app}
 
   :resources-path "resources/public"
+  :source-paths ["src/" "resources/templ/clojurescript/"]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+            [com.cemerick/austin "0.1.4"]]
 
   :cljsbuild {:builds [{
                         ;; The path to the top-level ClojureScript source directory:
@@ -18,12 +23,12 @@
                         ;; The standard ClojureScript compiler options:
                         ;; (See the ClojureScript compiler documentation for details.)
                         :compiler {
-                                   :output-to "resources/public/js/main.js"  ; default: target/cljsbuild-main.js
+                                   :output-to "resources/public/js/main.js"
                                    :optimizations :whitespace
                                    :pretty-print true}}]}
 
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]
+                                  [ring-mock "0.1.5"]
 
-                        [ring/ring-jetty-adapter "1.2.1"]
-                        [alembic "0.2.1"]]}})
+                                  [ring/ring-jetty-adapter "1.2.1"]
+                                  [alembic "0.2.1"]]}})
--- src/bkell/handler.clj
+++ src/bkell/handler.clj
@@ -1,9 +1,15 @@
 (ns bkell.handler
+  (:import  (javax.crypto Mac)
+            (javax.crypto.spec SecretKeySpec))
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [clojure.java.io :as io]
-            [ring.util.response :as ring-resp]))
+            [ring.util.response :as ring-resp]
+
+            [clojure.data.json :as json]
+            [clojure.data.codec.base64 :as b64]
+            [clojure.contrib.string :as cstring]))
 
 
 (defroutes app-routes
@@ -12,8 +18,31 @@
       (-> (ring-resp/response (slurp (io/resource "public/index.html")))
           (ring-resp/content-type "text/html")))
 
- #_(route/files "/" {:root "resources/public/"})
- #_(route/resources "/" {:root "resources/public/"})
+ (GET "/signedUploadParams" []
+
+      (let  [policy-doc "{ 'expiration': '2015-12-01T12:00:00.000Z' ,
+                           'conditions': [
+                                        {'bucket': 'bkeeping'},
+                                        ['starts-with', '$key', ''],
+                                        {'acl': 'public-read'}
+                                      ]
+                         }"
+
+             pdoc-filtered (cstring/replace-re #"\r" "" (cstring/replace-re #"\n" "" policy-doc))
+             policy (apply str (map char (-> pdoc-filtered .getBytes b64/encode)))
+
+             hmac-sha1 "HmacSHA1"
+             signing-key (SecretKeySpec. (.getBytes "EVnk7c840v0OouypchuzRnnq7qSbJMZLooDUtobL") hmac-sha1)
+             mac (doto (Mac/getInstance hmac-sha1) (.init signing-key))
+             signature (String. (b64/encode (.doFinal mac (.getBytes policy))))]
+
+        (json/json-str {
+                        :bucket "bkeeping"
+                        :key "${filename}"
+                        :AWSAccessKeyId "AKIAI7QL4ENX5KZ4QQCQ"
+                        :acl "public-read"
+                        :policy policy
+                        :signature signature})))
 
  (route/resources "/")
  (route/not-found "Not Found"))
