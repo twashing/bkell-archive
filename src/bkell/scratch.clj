02fbdafc20ff5cbf83100f943abee9799d1880ff
Timothy Washington <twashing@gmail.com>
2014-03-03 14:50:30 -0800

    -- working on workflow for creating a default set of books

---
+++ src/bkell/scratch.clj
@@ -0,0 +1,34 @@
+(ns bkell.scratch
+  (:require [bkell.spittoon :refer :all]
+            [bkell.init :as init]
+            [bkell.domain.books :as books]
+            [bkell.domain.accounts :as accounts]
+            [bkell.domain.journals :as journals]))
+
+
+(defn refresh []
+
+  (database-delete url)
+  (database-create url)
+  (alter-var-root conn (fn [inp] (database-connect url)))
+  (database-schema-create conn)
+
+
+  (init/init-db conn)
+
+
+  (def gresults (init/init-default-group conn))
+
+
+  ;; create accounts
+  (def aresults (accounts/create-default-accounts conn))
+
+
+  ;; create journal(s)
+  (def jresults (journals/create-journal conn "generalledger"))
+
+
+  ;; create books
+  (def bresults (books/create-books conn group-ref aresults jresults))
+
+)
