3c48dcd20c41f28fbaf42ed866f07d4e455ded62
Timothy Washington <twashing@gmail.com>
2014-03-03 06:54:29 -0800

    -- adding a list and find-by functions for journals

--- src/bkell/domain/journals.clj
+++ src/bkell/domain/journals.clj
@@ -4,6 +4,38 @@
             [bkell.domain.identity :as identity]))
 
 
+(defn list-journals [conn]
+
+  (let [query-expression '[:find ?e ?id ?name
+                           :where
+                           [?e :bookkeeping.group.books.journal/id ?id]
+                           [?e :bookkeeping.group.books.journal/name ?name]]
+        query-parameters []]
+    (spittoon/query query-expression query-parameters conn)))
+
+
+(defn find-journal-by-id [conn jid]
+
+  (let [query-expression '[:find ?e ?id ?name
+                           :in $ [?id]
+                           :where
+                           [?e :bookkeeping.group.books.journal/id ?id]
+                           [?e :bookkeeping.group.books.journal/name ?name]]
+        query-parameters [jid]]
+    (spittoon/query query-expression query-parameters conn)))
+
+
+(defn find-journal-by-name [conn jname]
+
+  (let [query-expression '[:find ?e ?id ?name
+                           :in $ [?name]
+                           :where
+                           [?e :bookkeeping.group.books.journal/id ?id]
+                           [?e :bookkeeping.group.books.journal/name ?name]]
+        query-parameters [jname]]
+    (spittoon/query query-expression query-parameters conn)))
+
+
 (defn generate-journal-nominal [jname]
 
   [{:db/id (d/tempid :db.part/user)
