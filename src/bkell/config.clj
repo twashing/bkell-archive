431106b73033b97f31567d4fdffd0397d814b22b
Timothy Washington <twashing@gmail.com>
2014-03-14 22:09:40 -0700

    -- readding config clj and edn files

---
+++ src/bkell/config.clj
@@ -0,0 +1,9 @@
+(ns bkell.config
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+(defn get-config-raw []
+  (let [pbreader (java.io.PushbackReader. (io/reader (io/resource "config.edn")))]
+    (edn/read pbreader)))
+
+(def get-config (memoize get-config-raw))
