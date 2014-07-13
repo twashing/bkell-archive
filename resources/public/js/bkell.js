26c9213d8dbbd5e19d43369730c630ae4e4e0124
Timothy Washington <twashing@interruptsoftware.com>
2014-07-13 03:26:31 +0000

    -- added some trickery to insert browser-repl code into a page being returned

--- resources/public/js/bkell.js
+++ resources/public/js/bkell.js
@@ -3,4 +3,4 @@ goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.objec
 goog.addDependency("../clojure/browser/event.js", ['clojure.browser.event'], ['cljs.core', 'goog.events.EventTarget', 'goog.events.EventType', 'goog.events']);
 goog.addDependency("../clojure/browser/net.js", ['clojure.browser.net'], ['goog.net.xpc.CfgFields', 'goog.net.XhrIo', 'goog.json', 'cljs.core', 'goog.net.EventType', 'goog.net.xpc.CrossPageChannel', 'clojure.browser.event']);
 goog.addDependency("../clojure/browser/repl.js", ['clojure.browser.repl'], ['cljs.core', 'clojure.browser.net', 'clojure.browser.event']);
-goog.addDependency("../bkell/core.js", ['bkell.core'], ['clojure.browser.repl', 'cljs.core']);
+//goog.addDependency("../bkell/core.js", ['bkell.core'], ['clojure.browser.repl', 'cljs.core']);
--- src/bkell/http/handler.clj
+++ src/bkell/http/handler.clj
@@ -3,13 +3,34 @@
             [compojure.route :as route]
             [compojure.handler :as handler]
             [net.cgrand.enlive-html :as enlive]
-            [ring.util.response :as ring-resp]))
+            [ring.util.response :as ring-resp]
+            [net.cgrand.enlive-html :as enlive]))
 
+
+(defn with-browser-repl [filename]
+
+  (let [templ (enlive/html-resource filename)
+        host "http://172.28.128.5"
+        port "44610"
+        connect-channel "4731"]
+
+    (apply str (enlive/emit*
+                (enlive/transform templ
+                                  [:html]
+                                  (enlive/after
+
+                                   ;; splitting up the tags to give time for 'clojure' JS object to load
+                                   [{:tag :script :content (str "goog.provide('bkell.core'); goog.require('cljs.core'); goog.require('clojure.browser.repl');")}
+                                    {:tag :script :content (str "clojure.browser.repl.connect.call(null,\"" host ":" port "/" connect-channel "/repl/start\");")}]))))))
 
 (defroutes app-routes
 
-  (GET "/" []
+  #_(GET "/" []
        (-> (ring-resp/file-response "index.html" {:root "resources/public"})
+           (ring-resp/content-type "text/html")))
+
+  (GET "/" []
+       (-> (ring-resp/response (with-browser-repl "index.html"))
            (ring-resp/content-type "text/html")))
 
   (route/resources "/" {:root "resources/public/"})
