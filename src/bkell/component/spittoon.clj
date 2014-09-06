76e68921c495c1ebde819b73981f75de43e2190b
Timothy Washington <twashing@interruptsoftware.com>
2014-09-06 22:50:06 +0000

    -- setting up schema & default values on startup

--- src/bkell/component/spittoon.clj
+++ src/bkell/component/spittoon.clj
@@ -8,14 +8,22 @@
 (defn db-load-schema [file-name]
   (cfg/load-edn file-name))
 
+(defn db-install-schema [data-store default-file]
+  (let [df (cfg/load-edn default-file)]
+    (adi/insert! data-store df)))
+
 (defn db-getconnection [env]
 
   (let [install-schema? true
         create-db? true
-        schema-file "db/schema-adi.edn"
-        db-url "datomic:mem://bkeeping"]
 
-    (adi/datastore db-url (db-load-schema schema-file) install-schema? create-db?)))
+        schema-file "db/schema-adi.edn"
+        default-file "db/default.edn"
+        db-url "datomic:mem://bkeeping"
+        ds (adi/datastore db-url (db-load-schema schema-file) install-schema? create-db?)]
+
+    (if create-db? (db-install-schema ds default-file))
+    ds))
 
 
 (defrecord Spittoon [env]
