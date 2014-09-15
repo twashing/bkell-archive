72d342e0ec127cd8a497bad0c338f4c28b2cc648
Timothy Washington <twashing@interruptsoftware.com>
2014-09-15 01:44:55 +0000

    -- cleaning up bkell.bkell

--- src/bkell/bkell.clj
+++ src/bkell/bkell.clj
@@ -36,48 +36,7 @@
   (refresh :after 'bkell.bkell/start))
 
 
-
-#_(defn ^{:doc "Initialize the bkeeping system"}
-  init
-  ([] (init :test))
-  ([env-key]
-
-     (let [rconfig (config/get-config-raw)
-
-           mode (:mode rconfig)
-           project-config (config/get-project-config mode rconfig)
-           loaded-system (fn [x] (cb/component-bkell project-config))]
-
-       (timbre/debug "INIT - project-config[" project-config "]")
-       (timbre/debug "INIT - loaded-system[" loaded-system "]")
-       (alter-var-root #'env project-config)
-       (alter-var-root #'system loaded-system))))
-
-
-#_(defn ^{:doc "Start the Bookkeeping Shell"}
-  start []
-  (if-not system
-    (init))
-  (alter-var-root #'system component/start))
-
-
-#_(defn ^{:doc "Stop the Bookkeeping Shell"}
-  stop []
-  (alter-var-root #'system
-                  (fn [s] (when s (component/stop s)))))
-
-#_(defn go
-  "Initializes the current development system and starts it running."
-  []
-  (init)
-  (start))
-
-#_(defn ^{:doc "Reset the mrservice system"}
-  reset []
-  (stop)
-  (refresh :after 'bkell.shell/go))
-
-#_(defn ^{:doc "This help function"}
+(defn ^{:doc "This help function"}
   help []
   (let [shell-members (mu/fns-in-ns 'bkell.bkell)
         extract-doc-fn (fn [msym]
@@ -92,4 +51,4 @@
                     (with-out-str (newline))]
                    (map extract-doc-fn shell-members)))))
 
-#_(defn ^{:doc "Reloads project configuration and libraries"} reload-project [] (reload-project))
+(defn ^{:doc "Reloads project configuration and libraries"} reload-project [] (reload-project))
